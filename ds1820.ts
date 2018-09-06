433bf3c0adeade5ad67d5706619bd8f3e922853f
DoraLC <34760377+DoraLC@users.noreply.github.com>
2018-09-06 15:12:39 +0800

    update

--- ds1820.ts
+++ ds1820.ts
@@ -1,48 +1,45 @@
-//% color=#2EFE2E
+enum Pins{
+  //% block="P0"
+  P0=  3,
+  //% block="P1"
+  P1=  2,
+  //% block="P2"
+  P2=  1,
+  //% block="P3"
+  P3=  4,
+  //% block="P4"
+  P4=  5,
+  //% block="P5"
+  P5=  17,
+  //% block="P6"
+  P6=  12,
+  //% block="P7"
+  P7=  11,
+  //% block="P8"
+  P8=  18,
+  //% block="P9"
+  P9=  10,
+  //% block="P10"
+  P10= 6,
+  //% block="P11"
+  P11= 26,
+  //% block="P12"
+  P12= 20,
+  //% block="P13"
+  P13= 23,
+  //% block="P14"
+  P14= 22,
+  //% block="P15"
+  P15= 21,
+  //% block="P16"
+  P16= 16,
+  //% block="P19"
+  P19= 0,
+  //% block="P20"
+  P20= 30
+}//% color=#2EFE2E
 //% icon="\uf2cb"
 namespace DS1820pxt {
-
-    enum Pins{
-      //% block="P0"
-      P0=  3,
-      //% block="P1"
-      P1=  2,
-      //% block="P2"
-      P2=  1,
-      //% block="P3"
-      P3=  4,
-      //% block="P4"
-      P4=  5,
-      //% block="P5"
-      P5=  17,
-      //% block="P6"
-      P6=  12,
-      //% block="P7"
-      P7=  11,
-      //% block="P8"
-      P8=  18,
-      //% block="P9"
-      P9=  10,
-      //% block="P10"
-      P10= 6,
-      //% block="P11"
-      P11= 26,
-      //% block="P12"
-      P12= 20,
-      //% block="P13"
-      P13= 23,
-      //% block="P14"
-      P14= 22,
-      //% block="P15"
-      P15= 21,
-      //% block="P16"
-      P16= 16,
-      //% block="P19"
-      P19= 0,
-      //% block="P20"
-      P20= 30
-    }
-
     /**
      * initialises local variablesssss
      */
